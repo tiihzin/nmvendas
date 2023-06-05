fc7117811fab46c9e999e306b20e46815e91092e
João Tiago <tig.j2025@gmail.com>
2023-06-05 19:09:05 -0300

    Edit Header

--- form.js
+++ form.js
@@ -1,18 +1,18 @@
 let pocket = document.querySelector('.menu-pocket');
-let navLinks = document.querySelector('.menu-nav');
+let pocketimg = document.querySelector('.img-menu');
+let navLinks = document.querySelector('.menu');
 let links =  document.querySelectorAll('.menu-link');
 let dark = document.querySelector('.darkness-body');
-let close = document.querySelector('.close');
 
 pocket.addEventListener('click', () => {
   navLinks.classList.toggle('hide');
+  if (navLinks.classList.contains('hide')) {
+    pocketimg.src = "src/header/Close.svg";
+  } else {
+    pocketimg.src = "src/header/HambugerMenu.svg"
+  }
   dark.classList.toggle('hiddem');
 });
-
-close.addEventListener('click', () => {
-  navLinks.classList.toggle('hide');
-  dark.classList.toggle('hiddem');
-})
 
 for ( i = 0; i < 3; i++) {
   links[i].addEventListener('click', () => {
