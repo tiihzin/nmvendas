a49f87419ff21a3de2209758cdf11d82511a601f
João Tiago <tig.j2025@gmail.com>
2023-06-05 17:59:04 -0300

    Edit Menu

--- form.js
+++ form.js
@@ -1,9 +1,23 @@
 let pocket = document.querySelector('.menu-pocket');
 let navLinks = document.querySelector('.menu-nav');
-let links =  document.querySelector('.menu-link');
+let links =  document.querySelectorAll('.menu-link');
 let dark = document.querySelector('.darkness-body');
+let close = document.querySelector('.close');
 
 pocket.addEventListener('click', () => {
   navLinks.classList.toggle('hide');
-  dark.classList.toggle('hiddem')
+  dark.classList.toggle('hiddem');
 });
+
+close.addEventListener('click', () => {
+  navLinks.classList.toggle('hide');
+  dark.classList.toggle('hiddem');
+})
+
+for ( i = 0; i < 3; i++) {
+  links[i].addEventListener('click', () => {
+    navLinks.classList.remove('hide');
+    dark.classList.toggle('hiddem');
+  });
+}
+
