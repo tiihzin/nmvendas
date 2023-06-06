22587e5de8c43b6251a23db2300237464bed1ec5
João Tiago <tig.j2025@gmail.com>
2023-06-06 01:52:22 -0300

    Edit files

--- form.js
+++ form.js
@@ -2,14 +2,17 @@ let pocket = document.querySelector('.menu-pocket');
 let pocketimg = document.querySelector('.img-menu');
 let navLinks = document.querySelector('.menu');
 let links =  document.querySelectorAll('.menu-link');
+let body = document.querySelector('.body');
 let dark = document.querySelector('.darkness-body');
 
 pocket.addEventListener('click', () => {
   navLinks.classList.toggle('hide');
   if (navLinks.classList.contains('hide')) {
     pocketimg.src = "src/header/Close.svg";
+    body.classList.add('no-scrol');
   } else {
-    pocketimg.src = "src/header/HambugerMenu.svg"
+    pocketimg.src = "src/header/HambugerMenu.svg";
+    body.classList.remove('no-scrol');
   }
   dark.classList.toggle('hiddem');
 });
@@ -20,8 +23,10 @@ for ( i = 0; i < 3; i++) {
     dark.classList.add('hiddem');
     if (navLinks.classList.contains('hide')) {
       pocketimg.src = "src/header/Close.svg";
+      body.classList.add('no-scrol');
     } else {
       pocketimg.src = "src/header/HambugerMenu.svg"
+      body.classList.remove('no-scrol');
     }
   });
 }
